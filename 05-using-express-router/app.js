eedc28f3b68ea696df9d8ed58b77fef6e6c0aadc
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-21 21:25:06 +0200

    served static files

--- 05-using-express-router/app.js
+++ 05-using-express-router/app.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const path = require('path');
-
+const rootDir = require('./util/path');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
