e62d4c078b87d797c54efb61944ae4b584880dcf
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-02-23 11:48:38 +0200

    added a layout for handlebars section

--- 03-pug-finished/app.js
+++ 03-pug-finished/app.js
@@ -1,29 +1,26 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs',expressHbs({
-   extname:'hbs',
-   layoutsDir: 'views/layouts/', 
-   defaultLayout: 'main-layout',
-})); //defualt layout path btw
+app.engine('hbs',expressHbs());
 app.set('view engine', 'hbs');
 app.set('views', 'views'); //default setting, but here as an e.i
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-   res.status(404).render('404', {docTitle: "404 Page Not Found"});
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000);
