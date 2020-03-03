f4c850fd0dd21dfe36326de8c16588f7dd6787ff
Mitchell Yuen <35192626+CodeNameNinja@users.noreply.github.com>
2020-03-03 16:19:42 +0200

    Connect to mySQL database and now you can fetch data from database

---
+++ SQL Section/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Mitch00788'
+});
+
+module.exports = pool.promise();
